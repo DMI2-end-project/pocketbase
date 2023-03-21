f3056354b38a326a8f6a0069c5531fc306044703
Maëva Mezzasalma <maeva@nother.io>
2023-03-21 10:08:14 +0100

    Add Dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+/*export default defineNuxtConfig({
 
-})
+})*/
+
+export default {
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  }
+}
