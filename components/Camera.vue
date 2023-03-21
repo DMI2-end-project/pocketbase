222fd90df8fb839c859f3fdd4d65a11f4f2c2e1d
Maëva Mezzasalma <maeva@nother.io>
2023-03-21 11:52:19 +0100

    Camera component

--- components/Camera.vue
+++ components/Camera.vue
@@ -1,10 +1,55 @@
 <template>
-  <video class="absolute w-full h-full top-0 left-0 object-cover object-center" autoPlay muted playsInline/>
+  <video ref="videoRef" class="absolute w-full h-full top-0 left-0 object-cover object-center"
+         autoPlay muted playsInline/>
 </template>
 
-<script>
-export default {
-  name: "Camera"
+<script setup>
+import {ref, onMounted, onBeforeUnmount} from 'vue';
+
+const videoRef = ref();
+
+onMounted(() => {
+  const constraints = {
+    audio: false,
+    video: {
+      facingMode: "environment"
+    }
+  };
+  console.log('window.navigator.mediaDevices', window.navigator, window.navigator.mediaDevices)
+
+  //Starting with Chrome 47, getUserMedia() requests are only allowed from secure origins: HTTPS or localhost.
+  if (window.navigator && window.navigator.mediaDevices) {
+    window.navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+  }
+})
+
+onBeforeUnmount(() => {
+  videoRef.value.pause();
+
+  const stream = getStream();
+  if (stream) {
+    const tracks = stream.getTracks();
+    for (const track of tracks) {
+      track.stop();
+    }
+  }
+
+  videoRef.value.srcObject = null;
+})
+
+const getStream = () => {
+  return (
+      (videoRef.value.srcObject instanceof MediaStream) ? videoRef.value.srcObject : null
+  );
+}
+const handleError = () => {
+}
+const handleSuccess = async (stream) => {
+  videoRef.value.srcObject = stream;
+  await videoRef.value.play();
+  /*if (onReady) {
+    onReady();
+  }*/
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <Camera ref="videoRef" :onReady="ready = true"/>
+ <h1>Test</h1>
+  <Camera ref="videoRef" style="width: 100%; border: #00dc82 1px solid" :onReady="ready = true"/>
   <ul>
     <li v-for="person in persons">
-      {{ person.firstname }}
+      {{ person ? person.firstname : '' }}
     </li>
   </ul>
 </template>
@@ -12,7 +13,6 @@ import * as tmImage from '@teachablemachine/image';
 import PocketBase from 'pocketbase';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
-
 
 import Camera from "../components/Camera";
 
@@ -30,37 +30,14 @@ export default {
     }
   },
   mounted() {
-    this.getPerson()
+    //this.getPerson()
   },
   methods: {
     async getPerson() {
       const result = await pb.collection('person').getList(1, 20);
       this.persons = result.items
     },
-    async update() {
-      this.frameRequestID = window.requestAnimationFrame(this.update);
-      let now = Date.now();
-      let delta = now - this.then;
-
-      if (delta > interval) {
-        this.then = now - (delta % interval);
-        await this.predict();
-      }
-    },
-    async predict() {
-      if (this.model && this.$refs.videoRef && this.$refs.videoRef.videoWidth) {
-        let prediction = await this.model.predict(this.$refs.videoRef);
-        prediction.forEach(predict => {
-          console.log('predict', predict.className)
-          if (predict.probability > .8 && predict.className !== 'Thrash') {
-            clearTimeout(timeout);
-            this.exp = predict.className;
-          } else if (!(predict.probability > .8 && predict.className === 'Thrash')) {
-            this.exp = '';
-          }
-        })
-      }
-    }
   }
 }
+/* */
 </script>
