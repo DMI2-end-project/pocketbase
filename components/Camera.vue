9a78e4019ffd43c5e90539ce42ce4cd2deacea4a
Maëva Mezzasalma <maeva@nother.io>
2023-03-21 08:59:33 +0100

    Add @teachablemachine/image and models

---
+++ components/Camera.vue
@@ -0,0 +1,13 @@
+<template>
+  <video class="absolute w-full h-full top-0 left-0 object-cover object-center" autoPlay muted playsInline/>
+</template>
+
+<script>
+export default {
+  name: "Camera"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
+  <Camera ref="videoRef" :onReady="ready = true"/>
   <ul>
     <li v-for="person in persons">
       {{ person.firstname }}
@@ -7,13 +8,24 @@
 </template>
 
 <script>
+import * as tmImage from '@teachablemachine/image';
 import PocketBase from 'pocketbase';
+
 const pb = new PocketBase('http://127.0.0.1:8090');
+
+
+import Camera from "../components/Camera";
+
 export default {
   name: "configurator",
-  components: {},
+  components: {
+    Camera
+  },
   data() {
     return {
+      frameRequestID: null,
+      model: null,
+      ready: false,
       persons: []
     }
   },
@@ -24,6 +36,30 @@ export default {
     async getPerson() {
       const result = await pb.collection('person').getList(1, 20);
       this.persons = result.items
+    },
+    async update() {
+      this.frameRequestID = window.requestAnimationFrame(this.update);
+      let now = Date.now();
+      let delta = now - this.then;
+
+      if (delta > interval) {
+        this.then = now - (delta % interval);
+        await this.predict();
+      }
+    },
+    async predict() {
+      if (this.model && this.$refs.videoRef && this.$refs.videoRef.videoWidth) {
+        let prediction = await this.model.predict(this.$refs.videoRef);
+        prediction.forEach(predict => {
+          console.log('predict', predict.className)
+          if (predict.probability > .8 && predict.className !== 'Thrash') {
+            clearTimeout(timeout);
+            this.exp = predict.className;
+          } else if (!(predict.probability > .8 && predict.className === 'Thrash')) {
+            this.exp = '';
+          }
+        })
+      }
     }
   }
 }
